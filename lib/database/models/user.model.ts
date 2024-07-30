057973b94cf7a24fa9db05190deff463e213ee91
dungthinhtech <dungthinhtech@gmail.com>
2024-07-30 19:21:37 +0700

    ss

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -3,8 +3,8 @@ import { Schema, model, models } from "mongoose";
 const UserSchema = new Schema({
   clerkId: {
     type: String,
-    required: true,
-    unique: true,
+    // required: true,
+    // unique: true,
   },
   email: {
     type: String,
